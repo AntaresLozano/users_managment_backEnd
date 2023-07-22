1d5e971ae9e1a7b752ba5cff43b8966dc30b4a9c
AntaresLozano <dizziness1948@gmail.com>
2023-07-22 18:06:23 -0500

    commiting app, users routes, migrations and controllers

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+        return $users;
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->apellidos = $request->apellidos;
+        $user->celular = $request->celular;
+        $user->direccion = $request->direccion;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->status = $request->status;
+        $user->genero = $request->genero;
+        $user->foto = $request->foto;
+        $user->email_verified_at = $request->email_verified_at;
+        $user->password = $request->password;
+        
+
+        $user->save();
+        return $user;
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return $user;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->apellidos = $request->apellidos;
+        $user->email = $request->email;
+        $user->celular = $request->celular;
+        $user->direccion = $request->direccion;
+        $user->role = $request->role;
+        $user->status = $request->status;
+        $user->genero = $request->genero;
+        $user->foto = $request->foto;
+
+        $user->save();
+        return $user;
+    }
+
+    public function destroy($id)
+    {
+       User::destroy($id);
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('apellidos');
+            $table->string('celular', 100);
+            $table->string('direccion', 100);
+            $table->string('email')->unique();
+            $table->string('role', 100);
+            $table->tinyInteger('status')->default(1);
+            $table->string('genero', 100);
+            $table->string('foto', 300);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
